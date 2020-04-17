0de8496a16df2a7e1bd6dafbb47420efb1aed3ac
sunnysunita <sunnysunita.com@gmail.com>
2020-04-17 23:32:25 +0530

    final

--- Reverse first K elements.py
+++ Reverse first K elements.py
@@ -1,5 +1,6 @@
 import queue
-
+from sys import setrecursionlimit
+setrecursionlimit(11000)
 def reverseQueue(q1):
     if q1.qsize() == 1:
         return
@@ -13,26 +14,43 @@ def reverseFirstK(q1, q2, k):
         q2.put(q1.get())
         k -= 1
 
-    reverseQueue(q2)
-    while q2.qsize() != 0:
-        q2.get()
+    """while q2.qsize() != 0:
+        print(q2.get())"""
     """while q1.qsize() != 0:
-        q2.put(q1.get())"""
+        print(q1.get())"""
+
+    reverseQueue(q2)
+    """while q1.qsize() != 0:
+        q1.get()"""
+    """while q2.qsize() != 0:
+        print(q2.get())"""
+
+    while q1.qsize() != 0:
+        q2.put(q1.get())
+    """while q2.qsize() != 0:
+        print(q2.get())"""
+    return q2
 
 
 #Implement Your Code Here
 
 
-from sys import setrecursionlimit
-setrecursionlimit(11000)
+"""from sys import setrecursionlimit
+setrecursionlimit(11000)"""
 n = int(input())
 li = [int(ele) for ele in input().split()]
+k = int(input())
 q = queue.Queue()
 q2 = queue.Queue()
 for ele in li:
     q.put(ele)
-k = int(input())
-reverseFirstK(q, q2, k)
-"""while(q.qsize()>0):
-    print(q.get())
-    n-=1"""
+
+"""while q.qsize() != 0:
+    print(q.get(), end=" ")"""
+
+out = reverseFirstK(q, q2, k)
+"""while out.qsize() != 0:
+    print(out.get())"""
+while(out.qsize()>0):
+    print(out.get())
+    n-=1
